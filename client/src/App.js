3715650a3854f04efcc133637f8a324e8d904903
bslzz <bishalkoid@gmail.com>
2021-02-12 18:31:53 +0200

    navbar and routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Customer from './pages/Customer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}>
+          Home
+        </Route>
+        <Route path="/register" component={Register}>
+          Register
+        </Route>
+        <Route path="/login" component={Login}>
+          Login
+        </Route>
+        <Route path="/customer" component={Customer}>
+          Customer
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
